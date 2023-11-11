a3c56301b6bbb08c7e86c2b6affa073084b86607
BimaAdi <bimaadi419@gmail.com>
2023-11-11 16:18:27 +0700

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-
+import { Button } from "@/client/components/ui/button";
+import { twMerge } from "tailwind-merge";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-screen grid place-items-center">
-      <h1>Hello From Next 14</h1>
+    <main className={twMerge("min-h-screen w-screen grid place-items-center")}>
+      <div className={twMerge("flex flex-col gap-2 justify-center")}>
+        <h1 className="text-xl">Hello From Next 14</h1>
+        <Button variant={"default"}>Click Me</Button>
+      </div>
     </main>
-  )
+  );
 }
